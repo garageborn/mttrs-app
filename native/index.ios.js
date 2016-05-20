c256c0ea328d4080de8fb7f0066c91aab330ca13
Andreas Diegues <andreas.diegues@gmail.com>
2016-05-20 01:28:48 -0300

    Stories + SafariView

--- native/index.ios.js
+++ native/index.ios.js
@@ -9,16 +9,60 @@ import {
   ScrollView,
   ListView,
   StatusBar,
-  Linking
+  Linking,
+  TouchableHighlight
 } from 'react-native';
+import SafariView from 'react-native-safari-view';
 import stories from './stories.json'
 
 class mttrs extends Component {
+  constructor(props) {
+    super(props);
+    StatusBar.setBarStyle('light-content')
+
+    this.dismissSubscription = () => {
+      StatusBar.setBarStyle("light-content");
+    }
+
+    SafariView.addEventListener("onDismiss", this.dismissSubscription)
+  }
+
+  get dataSource() {
+    let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+    return ds.cloneWithRows(stories.slice(0, 20))
+  }
+
+  openStory(storyURL) {
+    SafariView.isAvailable()
+      .then(
+        SafariView.show({ url: storyURL, readerMode: true })
+      )
+      .then(
+        StatusBar.setBarStyle('default')
+      )
+      .catch(error => {
+        // iOS 8 - Fuck it?
+        Linking.openURL(storyURL)
+      });
+  }
+
+  renderRow(rowData, sectionId, rowId) {
+    return (
+      <TouchableHighlight activeOpacity={0.7} underlayColor='white' onPress={this.openStory.bind(this, rowData.url)}>
+        <View style={styles.story}>
+          <Image source={{uri: rowData.image.thumb}} style={styles.storyThumb} />
+          <View style={styles.storyTitleContainer}>
+            <Text numberOfLines={3}>{rowData.title}</Text>
+            <Text style={styles.storyInfo}>@4AM <Text style={styles.storyInfoFrom}>from</Text> {rowData.publisher.name}</Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar barStyle="light-content" />
-
         <View>
           <View style={styles.header}>
             <Image source={require('./img/mttrs.png')} style={styles.logo} />
@@ -48,26 +92,9 @@ class mttrs extends Component {
           </ScrollView>
         </View>
 
-        <ListView dataSource={this.dataSource} renderRow={this.renderRow} style={styles.storyList} />
+        <ListView dataSource={this.dataSource} renderRow={this.renderRow.bind(this)} style={styles.storyList} />
       </View>
     );
-  }
-
-  get dataSource() {
-    let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
-    return ds.cloneWithRows(stories.slice(0, 20))
-  }
-
-  renderRow(rowData, sectionId, rowId) {
-    return (
-      <View style={styles.story}>
-        <Image source={{uri: rowData.image.thumb}} style={styles.storyThumb} />
-        <View style={styles.storyTitleContainer}>
-          <Text numberOfLines={2} onPress={() => Linking.openURL(rowData.url)}>{rowData.title}</Text>
-          <Text style={styles.storyInfo}>@4AM <Text style={styles.storyInfoFrom}>from</Text> {rowData.publisher.name}</Text>
-        </View>
-      </View>
-    )
   }
 }
 
