1622d3ae3a49ab11facdf792248feb67088b0beb
Alexandre Bini <alexandrebini@gmail.com>
2016-06-07 12:41:40 -0300

    Features/fix server (#9)
    * Fix server
    
    * Fix server
    
    * Fix server
    
    * Fix server
    
    * Fix server
    
    * Fix server
    
    * Fix server
    
    * Fix server
    
    * Fix server
    
    * Fix server
    
    * Fix server
    
    * Fix server
    
    * devtools
    
    * devtools
    
    * devtools

--- app/web/utils/Cloudinary.js
+++ app/web/utils/Cloudinary.js
@@ -2,7 +2,7 @@ import React from 'react'
 import cloudinary from 'cloudinary-core'
 
 const cl = cloudinary.Cloudinary.new()
-cl.config('cloud_name', 'dt7iowhph')
+cl.config('cloud_name', 'dwbqes7mx')
 
 const defaultOptions = { fetch_format: 'auto', dpr: 2 }//cl.device_pixel_ratio() }
 
