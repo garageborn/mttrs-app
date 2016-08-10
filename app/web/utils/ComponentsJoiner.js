9353c968495b737d28320d3936a347adc2ce7d0e
Lucas <lucas@agtlucas.com>
2016-08-10 17:50:40 -0300

    Change Math.random() to => _.uniqueid() on ComponentsJoiner

--- app/web/utils/ComponentsJoiner.js
+++ app/web/utils/ComponentsJoiner.js
@@ -1,23 +1,24 @@
 import React from 'react'
+import _ from 'lodash'
 
 const ComponentsJoiner = (componentArr) => {
   return componentArr.map((component, index) => {
     if (index < componentArr.length - 2) {
       return (
-        <div key={Math.random().toString(16)}>
+        <div key={_.uniqueid('c_')}>
           {component}<span>,&nbsp;</span>
         </div>
       )
     }
     if (index === componentArr.length - 2) {
       return (
-        <div key={Math.random().toString(16)}>
+        <div key={_.uniqueid('c_')}>
           {component}<span>&nbsp;and&nbsp;</span>
         </div>
       )
     }
     return (
-      <div key={Math.random().toString(16)}>
+      <div key={_.uniqueid('c_')}>
         {component}
       </div>
     )
