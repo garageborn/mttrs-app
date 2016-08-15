744d19004427406564b5ed1222ad0330e5ed6745
Lucas <lucas@agtlucas.com>
2016-08-15 13:52:14 -0300

    Migrating RN

--- app/native/styles/app.ios.js
+++ app/native/styles/app.ios.js
@@ -1,4 +1,5 @@
-import React, {StyleSheet} from 'react-native'
+import React from 'react'
+import {StyleSheet} from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
---
+++ index.ios.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import App from './app/native/containers/App'
+
+AppRegistry.registerComponent('mttrs', () => App)
