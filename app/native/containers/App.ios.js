bf46c216b90bc70e23be6812172879bcabb51078
Lucas <lucas@agtlucas.com>
2016-08-17 10:47:54 -0300

    Update app container and header

--- app/native/containers/App.ios.js
+++ app/native/containers/App.ios.js
@@ -8,10 +8,10 @@ import {
 } from 'react-native'
 import SafariView from 'react-native-safari-view'
 import styles from '../styles/app'
-import StoryList from './StoryList'
-import {Provider} from 'react-redux'
-import configureStore from '../store/configureStore'
-const store = configureStore()
+// import StoryList from './StoryList'
+// import {Provider} from 'react-redux'
+// import configureStore from '../store/configureStore'
+// const store = configureStore()
 
 export default class App extends Component {
   constructor(props) {
@@ -27,40 +27,33 @@ export default class App extends Component {
 
   render() {
     return (
-      <Provider store={store}>
+      // <Provider store={store}>
         <View style={styles.container}>
           <View>
             <View style={styles.header}>
-              <Image source={require('mttrs/app/native/assets/mttrs.png')} style={styles.logo} />
+              <Image source={require('mttrs/app/native/assets/logo.png')} style={styles.logo} />
             </View>
 
             <ScrollView style={styles.nav} contentContainerStyle={styles.navContainer} horizontal={true} showsHorizontalScrollIndicator={false}>
-              <Image source={require('mttrs/app/native/assets/bullet.png')} style={styles.navBullet} />
               <Text style={styles.navItem}>All</Text>
 
-              <Image source={require('mttrs/app/native/assets/bullet.png')} style={styles.navBullet} />
               <Text style={styles.navItem} style={styles.navItem} style={styles.navItem} style={styles.navItem}>World News</Text>
 
-              <Image source={require('mttrs/app/native/assets/bullet.png')} style={styles.navBullet} />
               <Text style={styles.navItem} style={styles.navItem} style={styles.navItem}>Business</Text>
 
-              <Image source={require('mttrs/app/native/assets/bullet.png')} style={styles.navBullet} />
               <Text style={styles.navItem} style={styles.navItem}>Technlogy</Text>
 
-              <Image source={require('mttrs/app/native/assets/bullet.png')} style={styles.navBullet} />
               <Text style={styles.navItem}>Entertainment</Text>
 
-              <Image source={require('mttrs/app/native/assets/bullet.png')} style={styles.navBullet} />
               <Text style={styles.navItem}>Humor</Text>
 
-              <Image source={require('mttrs/app/native/assets/bullet.png')} style={styles.navBullet} />
               <Text style={styles.navItem}>Science</Text>
             </ScrollView>
           </View>
 
-          <StoryList />
+          {/* <StoryList /> */}
         </View>
-      </Provider>
+      // </Provider>
     )
   }
 }
