a9014f039071c75212a8ffd1745145b092b5c9ee
Lucas <lucas@agtlucas.com>
2016-08-18 14:33:04 -0300

    Break components

--- app/native/components/Story.js
+++ app/native/components/Story.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import {
   Image,
   Text,
@@ -7,27 +7,24 @@ import {
 } from 'react-native'
 import styles from '../styles/app'
 
-class Story extends Component {
-  render() {
-    const {story, onClick} = this.props
-    return (
-      <TouchableHighlight activeOpacity={0.7} underlayColor='white' onPress={onClick.bind(this, story)}>
-        <View style={styles.story}>
-          <Image source={{uri: story.image.thumb}} style={styles.storyThumb} />
-          <View style={styles.storyTitleContainer}>
-            <Text numberOfLines={3}>{story.title}</Text>
-            <Text style={styles.storyInfo}>@4AM <Text style={styles.storyInfoFrom}>from</Text> {story.publisher.name}</Text>
-          </View>
+const Story = ({onClick, thumb, title, name}) => {
+  return (
+    <TouchableHighlight activeOpacity={0.7} underlayColor='white' onPress={onClick}>
+      <View style={styles.story}>
+        <Image source={{uri: thumb}} style={styles.storyThumb} />
+        <View style={styles.storyTitleContainer}>
+          <Text numberOfLines={3}>{title}</Text>
+          <Text style={styles.storyInfo}>@4AM <Text style={styles.storyInfoFrom}>from</Text> {name}</Text>
         </View>
-      </TouchableHighlight>
-    )
-  }
+      </View>
+    </TouchableHighlight>
+  )
 }
 
 Story.propTypes = {
   story: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
   }).isRequired,
   onClick: PropTypes.func.isRequired
 }
--- app/native/containers/App.ios.js
+++ app/native/containers/App.ios.js
@@ -4,56 +4,33 @@ import {
   ScrollView,
   StatusBar,
   Text,
-  View,
+  View
 } from 'react-native'
 import SafariView from 'react-native-safari-view'
 import styles from '../styles/app'
+import Header from '../components/Header'
 // import StoryList from './StoryList'
 // import {Provider} from 'react-redux'
 // import configureStore from '../store/configureStore'
 // const store = configureStore()
 
 export default class App extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     StatusBar.setBarStyle('light-content')
 
     this.dismissSubscription = () => {
-      StatusBar.setBarStyle("light-content")
+      StatusBar.setBarStyle('light-content')
     }
 
-    SafariView.addEventListener("onDismiss", this.dismissSubscription)
+    SafariView.addEventListener('onDismiss', this.dismissSubscription)
   }
 
-  render() {
+  render () {
     return (
-      // <Provider store={store}>
-        <View style={styles.container}>
-          <View>
-            <View style={styles.header}>
-              <Image source={require('mttrs/app/native/assets/logo.png')} style={styles.logo} />
-            </View>
-
-            <ScrollView style={styles.nav} contentContainerStyle={styles.navContainer} horizontal={true} showsHorizontalScrollIndicator={false}>
-              <Text style={styles.navItem}>All</Text>
-
-              <Text style={styles.navItem} style={styles.navItem} style={styles.navItem} style={styles.navItem}>World News</Text>
-
-              <Text style={styles.navItem} style={styles.navItem} style={styles.navItem}>Business</Text>
-
-              <Text style={styles.navItem} style={styles.navItem}>Technlogy</Text>
-
-              <Text style={styles.navItem}>Entertainment</Text>
-
-              <Text style={styles.navItem}>Humor</Text>
-
-              <Text style={styles.navItem}>Science</Text>
-            </ScrollView>
-          </View>
-
-          {/* <StoryList /> */}
-        </View>
-      // </Provider>
+      <View style={styles.container}>
+        <Header />
+      </View>
     )
   }
 }
