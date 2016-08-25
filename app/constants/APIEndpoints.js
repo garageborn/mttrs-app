e9c9bdc8307d5567f28724abed2a64c431fdda5b
Alexandre Bini <alexandrebini@gmail.com>
2016-08-25 16:31:41 -0300

    ssl

--- app/constants/APIEndpoints.js
+++ app/constants/APIEndpoints.js
@@ -1,4 +1,4 @@
-const ROOT = process.env.MTTRS_API_URL || 'http://api.mtt.rs'
+const ROOT = process.env.MTTRS_API_URL || 'https://api.mtt.rs'
 export const CATEGORIES = `${ROOT}/categories.json`
 export const PUBLISHERS = `${ROOT}/publishers.json`
 export const STORIES = `${ROOT}/stories.json`
