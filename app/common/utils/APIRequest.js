6a0ef08e9a59936a1ccd276f872233a8ae28f2a3
Alexandre Bini <alexandrebini@gmail.com>
2016-09-20 10:32:36 -0300

    debug tenant

--- app/common/utils/APIRequest.js
+++ app/common/utils/APIRequest.js
@@ -2,5 +2,6 @@ import request from 'superagent'
 import Tenant from './Tenant'
 
 export default function(method, url) {
+  console.log('----------', Tenant.current, url)
   return request(method, url).set('X-Tenant', Tenant.current)
 }
