cbf2b47e7b889a4d3eee106c643066ff3bdbdab4
Alexandre Bini <alexandrebini@gmail.com>
2016-09-20 15:10:30 -0300

    starting

---
+++ app/native/containers/MenuContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+
+class MenuContainer extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Batata</Text>
+      </View>
+    )
+  }
+}
+
+export default MenuContainer
