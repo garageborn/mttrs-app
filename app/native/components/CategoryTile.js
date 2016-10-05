0d4b96551164308b61d096e7e6c1568232f65054
AgtLucas <lucas@agtlucas.com>
2016-10-05 15:24:56 -0300

    ES6...

--- app/native/components/CategoryTile.js
+++ app/native/components/CategoryTile.js
@@ -12,7 +12,7 @@ class CategoryTile extends Component {
       <TouchableHighlight style={styles.categoryTouch} onPress={e => onPress(category)}>
         <View style={this.categoryStyle} shadowOffset={{width: 1, height: 2}} shadowColor={'rgba(0, 0, 0, .1)'} shadowOpacity={1.0}>
           <Image style={styles.categoryIcon} source={{uri: this.categoryIcon}} />
-          <Text style={[styles.categoryName, {color: color}]}>{name}</Text>
+          <Text style={[styles.categoryName, {color}]}>{name}</Text>
         </View>
       </TouchableHighlight>
     )
