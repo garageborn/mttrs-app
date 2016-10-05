9b148d29f75252996a2338ca98aeb1ca784c1dc1
Lucas <AgtLucas@users.noreply.github.com>
2016-10-05 15:26:35 -0300

    Update category menu (#59)
    * Update category menu
    
    * ES6...

--- app/native/components/CategoryTile.js
+++ app/native/components/CategoryTile.js
@@ -1,24 +1,33 @@
 import React, { Component, PropTypes } from 'react'
 import { View, Text, Image, TouchableHighlight } from 'react-native'
 import styles from '../styles/Menu'
+import * as cloudinary from '../../common/utils/Cloudinary'
 
 class CategoryTile extends Component {
   render() {
     const { onPress, category } = this.props
     const { name, color, slug } = category
-    let icon = require('../assets/icons/icon-business.png')
+
     return (
       <TouchableHighlight style={styles.categoryTouch} onPress={e => onPress(category)}>
         <View style={this.categoryStyle} shadowOffset={{width: 1, height: 2}} shadowColor={'rgba(0, 0, 0, .1)'} shadowOpacity={1.0}>
-          <Image style={styles.categoryIcon} source={icon} />
-          <Text style={[styles.categoryName, {color: '#373737'}]}>{name}</Text>
+          <Image style={styles.categoryIcon} source={{uri: this.categoryIcon}} />
+          <Text style={[styles.categoryName, {color}]}>{name}</Text>
         </View>
       </TouchableHighlight>
     )
   }
 
+  get categoryIcon() {
+    const { category } = this.props
+    let options = { secure: true }
+
+    return cloudinary.id(category.icon_id, options)
+  }
+
   get categoryStyle() {
-    return this.props.isActive ? [styles.category, styles.isActive] : styles.category
+    const { category } = this.props
+    return this.props.isActive ? [styles.category, styles.isActive, { borderColor: category.color }] : styles.category
   }
 }
 
