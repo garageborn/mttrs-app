ec0fed03eba0a2ea97edc0c63da75d31cd5dbd47
Klaus Kohut <klausy@gmail.com>
2016-10-13 15:46:33 -0300

    Migrate menu to TimelineContainer

--- app/native/containers/TimelineContainer.js
+++ app/native/containers/TimelineContainer.js
@@ -1,13 +1,16 @@
 import React, { Component, PropTypes } from 'react'
-import { View } from 'react-native'
+import { View, Animated, Dimensions } from 'react-native'
 import { connect } from 'react-redux'
 import { NavigationActions } from '@exponent/ex-navigation'
-import { TimelineActions } from '../actions/index'
+import { TimelineActions, MenuActions } from '../actions/index'
 import Timeline from '../components/Timeline'
 import StoryContainer from './StoryContainer'
 import StoryLinksContainer from './StoryLinksContainer'
 import Router from '../config/Router'
 import styles from '../styles/App'
+import MenuContainer from './MenuContainer'
+
+const { height } = Dimensions.get('window')
 
 class TimelineContainer extends Component {
   constructor(props) {
@@ -15,6 +18,10 @@ class TimelineContainer extends Component {
     this.onEndReached = this.onEndReached.bind(this)
     this.onPullToRefresh = this.onPullToRefresh.bind(this)
     this.renderStory = this.renderStory.bind(this)
+    this.closeMenu = this.closeMenu.bind(this)
+    this.state = {
+      menuPositionY: new Animated.Value(-height)
+    }
   }
 
   componentDidMount() {
@@ -28,7 +35,8 @@ class TimelineContainer extends Component {
     let sectionNameChanged = nextSection.name !== currentSection.name
     let sectionModelChanged = nextSection.model !== currentSection.model
     if (sectionNameChanged || sectionModelChanged) this.fetchData(nextProps)
-
+    if (nextProps.uiReducer.menu.isOpen) this.animate('in')
+    if (nextProps.uiReducer.menu.retract) this.animate('out')
   }
 
   fetchData(props) {
@@ -65,11 +73,28 @@ class TimelineContainer extends Component {
     this.infiniteFetchData(this.props)
   }
 
+  animate(type) {
+    const value = type === 'in' ? 0 : -height;
+    const callback = type === 'out' ? this.closeMenu : null
+    return (
+      Animated.timing(
+        this.state.menuPositionY,
+        {
+          toValue: value,
+          duration: 330
+        }
+      ).start(callback)
+    )
+  }
+
   render() {
     const { items, isFetching, isFetchingTop } = this.props
     return (
       <View style={styles.container}>
         {this.renderStoryLinks()}
+        <Animated.View style={{transform: [{translateY: this.state.menuPositionY}]}}>
+          { this.renderMenu() }
+        </Animated.View>
         <Timeline
           items={items}
           isFetching={isFetching}
@@ -80,6 +105,15 @@ class TimelineContainer extends Component {
           />
       </View>
     )
+  }
+
+  renderMenu() {
+    const { params, uiReducer } = this.props
+    if (uiReducer.menu.isOpen) return <MenuContainer params={params}/>
+  }
+
+  closeMenu() {
+    this.props.dispatch(MenuActions.closeMenu())
   }
 
   renderStory(story) {
@@ -99,7 +133,8 @@ let mapStateToProps = (state) => {
   return {
     items: state.TimelineReducers.items,
     isFetching: state.TimelineReducers.isFetching,
-    isFetchingTop: state.TimelineReducers.isFetchingTop
+    isFetchingTop: state.TimelineReducers.isFetchingTop,
+    uiReducer: state.uiReducer
   }
 }
 
