98cbac42cf958ffee382792210c3f601b92d1233
Lucas <AgtLucas@users.noreply.github.com>
2016-10-20 21:47:44 -0200

    New selected category style (#90)

--- app/native/components/CategoryTile.js
+++ app/native/components/CategoryTile.js
@@ -12,8 +12,8 @@ class CategoryTile extends Component {
       <View style={styles.categoryContainer}>
         <TouchableHighlight underlayColor={'rgba(255,255,255,.1)'} onPress={e => onPress(category)}>
           <View style={this.categoryStyle} shadowOffset={{width: 0, height: 2}} shadowColor={'rgba(0, 0, 0, 1)'} shadowOpacity={.5} elevation={1}>
-            { this.renderIcon() }
-            <Text style={[styles.categoryName, {color}]}>{name}</Text>
+            {this.renderIcon()}
+            <Text style={this.categoryNameStyle}>{name}</Text>
           </View>
         </TouchableHighlight>
       </View>
@@ -21,15 +21,34 @@ class CategoryTile extends Component {
   }
 
   renderIcon() {
-    const { category } = this.props
+    const { category, isActive } = this.props
+
     if (!category.icon_id) return
-    const uri = cloudinary.id(category.icon_id, { secure: true })
+
+    let options = {
+      secure: true
+    }
+
+    if (isActive) {
+      options = {
+        effect: 'colorize',
+        color: '#FFF'
+      }
+    }
+
+    let uri = cloudinary.id(category.icon_id, options)
     return <Image style={styles.categoryIcon} source={{uri: uri}} />
   }
 
   get categoryStyle() {
-    const { category } = this.props
-    return this.props.isActive ? [styles.category, styles.isActive, { borderColor: category.color }] : styles.category
+    const { category, isActive } = this.props
+    return isActive ? [styles.category, { backgroundColor: category.color }] : styles.category
+  }
+
+  get categoryNameStyle() {
+    const { category, isActive } = this.props
+    const { color } = category
+    return isActive ? [styles.categoryName, { color: '#FFF' }] : [styles.categoryName, { color }]
   }
 }
 
