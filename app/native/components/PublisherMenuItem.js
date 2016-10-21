9eaeb8fd075ec604ae3fe16cfaec1b5a3010945d
Lucas <AgtLucas@users.noreply.github.com>
2016-10-21 15:05:32 -0200

    TouchableHighlight colors (#91)
    * Add and remove some colors
    
    * Add TRANSPARENT constant

--- app/native/components/PublisherMenuItem.js
+++ app/native/components/PublisherMenuItem.js
@@ -3,12 +3,16 @@ import { View, Text, TouchableHighlight, Image } from 'react-native'
 import PublisherLogo from './PublisherLogo'
 import styles from '../styles/MenuPublishers'
 import * as cloudinary from '../../common/utils/Cloudinary'
+import { WHITE_TRANSPARENT_COLOR } from '../../constants/TouchUnderlayColors'
 
 class PublisherMenuItem extends Component {
   render() {
     const { onPress, publisher } = this.props
     return (
-      <TouchableHighlight style={styles.touch} onPress={e => onPress(publisher)}>
+      <TouchableHighlight
+        style={styles.touch}
+        onPress={e => onPress(publisher)}
+        underlayColor={WHITE_TRANSPARENT_COLOR}>
         <View style={styles.publisher}>
           {this.renderIcon()}
           <Text style={styles.name}>{publisher.name}</Text>
