ef2390f1b3defc8af4105b6680f708d284c16ea0
Lucas <AgtLucas@users.noreply.github.com>
2016-10-28 09:36:04 -0200

    Webview progress bar (#96)
    * WIP - Progressbar
    
    * Update progress bar
    
    * Update progressbar
    
    * Remove redux connect
    
    * Update progressbar
    
    * Improve calc
    
    * Kill console.log

---
+++ app/native/components/ProgressBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+  StyleSheet,
+  ProgressViewIOS,
+  ProgressBarAndroid,
+  Platform
+} from 'react-native'
+
+const ProgressBar = ({ progress, color }) => {
+  if (Platform.OS === 'ios') {
+    return <ProgressViewIOS style={styles.progressBar} progress={progress} progressTintColor={color} />
+  } else {
+    return <ProgressBarAndroid style={styles.progressBar} progress={progress} indeterminate={false} styleAttr='Horizontal' color={color} />
+  }
+}
+
+const styles = StyleSheet.create({
+  progressBar: {
+    marginTop: Platform.OS === 'ios' ? 11 : -6
+  }
+})
+
+export default ProgressBar
--- app/native/containers/LinkSceneContainer.js
+++ app/native/containers/LinkSceneContainer.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
-import { View, WebView, Platform } from 'react-native'
+import { View, WebView, Platform, Animated } from 'react-native'
 import LinkHeaderContainer from './LinkHeaderContainer'
+import ProgressBar from '../components/ProgressBar'
 import styles from '../styles/App'
-import { connect } from 'react-redux'
 
 class LinkSceneContainer extends Component {
+  static progress
+
   static route = {
     navigationBar: {
       ...Platform.select({
         ios: {
           renderLeft: () => <View />,
-          renderTitle: (route) => <LinkHeaderContainer link={route.params.link}/>,
+          renderTitle: (route) => <LinkHeaderContainer link={route.params.link} />,
           backgroundColor: '#262C5B'
         },
         android: {
@@ -18,6 +20,33 @@ class LinkSceneContainer extends Component {
         }
       })
     }
+  }
+
+  constructor() {
+    super()
+    this.state = {
+      progress: 0
+    }
+  }
+
+  componentDidMount() {
+    this.progressLoading()
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.constructor.progress)
+  }
+
+  progressLoading() {
+    this.constructor.progress = setInterval(() => {
+      this.setState({
+        progress: this.state.progress === 1 ? 1 : Math.min(0.95, this.state.progress + 0.01)
+      })
+    }, 50)
+  }
+
+  renderProgressBar = () => {
+    return <ProgressBar progress={this.state.progress} color='#2672D7' />
   }
 
   render() {
@@ -29,7 +58,12 @@ class LinkSceneContainer extends Component {
         {Platform.OS === 'android' &&
           <LinkHeaderContainer link={this.props.route.params.link} />
         }
-        <WebView source={{uri: url}} contentInset={{top: 11}}/>
+        <WebView
+          source={{uri: url}}
+          contentInset={{top: 11}}
+          startInLoadingState={true}
+          renderLoading={this.renderProgressBar}
+          />
       </View>
     )
   }
