6b819cf1080ebc99226af491ec3c33cc5be09ce3
AgtLucas <lucas@agtlucas.com>
2016-10-28 09:12:52 -0200

    Update progressbar

--- app/native/containers/LinkSceneContainer.js
+++ app/native/containers/LinkSceneContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { View, WebView, Platform, ProgressViewIOS, ProgressBarAndroid } from 'react-native'
+import { View, WebView, Platform, Animated } from 'react-native'
 import LinkHeaderContainer from './LinkHeaderContainer'
+import ProgressBar from '../components/ProgressBar'
 import styles from '../styles/App'
 
 class LinkSceneContainer extends Component {
@@ -11,7 +12,7 @@ class LinkSceneContainer extends Component {
       ...Platform.select({
         ios: {
           renderLeft: () => <View />,
-          renderTitle: (route) => <LinkHeaderContainer link={route.params.link}/>,
+          renderTitle: (route) => <LinkHeaderContainer link={route.params.link} />,
           backgroundColor: '#262C5B'
         },
         android: {
@@ -41,20 +42,16 @@ class LinkSceneContainer extends Component {
       this.setState({
         progress: this.state.progress === 1 ? 1 : Math.min(1, this.state.progress + 0.01)
       })
-    }, 17 * 2)
+    }, 20 * 2)
   }
 
   renderProgressBar = () => {
-    if (Platform.OS === 'ios') {
-      return <ProgressViewIOS style={{marginTop: 11}} progress={this.state.progress} progressTintColor={'#2672D7'} />
-    } else {
-      return <ProgressBarAndroid style={{marginTop: -6}} progress={this.state.progress} indeterminate={false} styleAttr='Horizontal' color='#2672D7' />
-    }
+    return <ProgressBar progress={this.state.progress} color='#2672D7' />
   }
 
   render() {
     const { url } = this.props.route.params.link
-
+    console.log(this.state.progress)
     return (
       <View style={styles.container}>
         {/* Waiting for the new ex-navigation release which will fix this */}
