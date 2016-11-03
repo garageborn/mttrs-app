e8747ac4c955b6cb7fd8ae673e11edc1e2179b85
AgtLucas <lucas@agtlucas.com>
2016-11-03 07:50:36 -0200

    Improve timeline header

--- app/native/containers/TimelineSceneContainer.js
+++ app/native/containers/TimelineSceneContainer.js
@@ -2,20 +2,17 @@ import React, { Component } from 'react'
 import { View } from 'react-native'
 import TimelineHeaderContainer from './TimelineHeaderContainer'
 import TimelineContainer from './TimelineContainer'
+import styles from '../styles/App'
 
 class TimelineSceneContainer extends Component {
-  static route = {
-    navigationBar: {
-      renderTitle: (route) => <TimelineHeaderContainer params={route.params}/>,
-      renderLeft: () => <View />,
-      renderRight: () => <View />,
-      backgroundColor: '#262C5B'
-    }
-  }
-
   render() {
     const { route } = this.props
-    return <TimelineContainer params={route.params}/>
+    return (
+      <View style={styles.container}>
+        <TimelineHeaderContainer params={route.params} />
+        <TimelineContainer params={route.params} />
+      </View>
+    )
   }
 }
 
