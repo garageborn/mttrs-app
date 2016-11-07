7f545810b22a67598ebbe1d34b0dfad403717fb9
AgtLucas <lucas@agtlucas.com>
2016-11-07 09:40:36 -0200

    Add env, update sentry

--- android/app/src/main/java/com/mttrs/MainApplication.java
+++ android/app/src/main/java/com/mttrs/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,8 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSharePackage(),
-            new LinearGradientPackage()
+          new RNSharePackage(),
+          new LinearGradientPackage(),
+          new ReactNativeConfigPackage()
       );
     }
   };
