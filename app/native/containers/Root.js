0f170c68b012f76cc7012d1132f4bdc120f7fb80
AgtLucas <lucas@agtlucas.com>
2016-11-07 18:11:19 -0200

    Mock Sentry

--- app/native/containers/Root.js
+++ app/native/containers/Root.js
@@ -1,9 +1,20 @@
 import React, { Component, PropTypes } from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { StatusBar, Platform } from 'react-native'
+import Raven from 'raven-js'
+import Config from 'react-native-config'
 import { NavigationContext, NavigationProvider, StackNavigation, } from '@exponent/ex-navigation'
 import Router from '../config/Router'
 import apolloClient from '../config/apolloClient'
+require('raven-js/plugins/react-native')(Raven)
+
+if (!__DEV__) {
+  const DSN_KEY = 'https://5cdede7e751f4807b1113013db2d917b@sentry.io/87957'
+
+  Raven
+    .config(Config.DSN_KEY, { release: '1.0' })
+    .install()
+}
 
 class Root extends Component {
   render() {
