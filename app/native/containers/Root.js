c2722a51e19108bb56cd647db727fa10c4a53666
AgtLucas <lucas@agtlucas.com>
2016-11-07 18:09:19 -0200

    Remove Sentry, for now

--- app/native/containers/Root.js
+++ app/native/containers/Root.js
@@ -1,18 +1,9 @@
 import React, { Component, PropTypes } from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { StatusBar, Platform } from 'react-native'
-import Raven from 'raven-js'
-import Config from 'react-native-config'
 import { NavigationContext, NavigationProvider, StackNavigation, } from '@exponent/ex-navigation'
 import Router from '../config/Router'
 import apolloClient from '../config/apolloClient'
-require('raven-js/plugins/react-native')(Raven)
-
-if (!__DEV__) {
-  Raven
-    .config(Config.DSN_KEY, { release: '1.0' })
-    .install()
-}
 
 class Root extends Component {
   render() {
