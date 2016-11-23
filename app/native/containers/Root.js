8d0d2c0d8bf63005322d8297cd7fe97cd1943243
AgtLucas <lucas@agtlucas.com>
2016-11-22 22:33:08 -0200

    WIP - i18n

--- app/native/containers/Root.js
+++ app/native/containers/Root.js
@@ -1,11 +1,18 @@
 import React, { Component, PropTypes } from 'react'
+import { StatusBar, Platform, NativeModules } from 'react-native'
 import { ApolloProvider } from 'react-apollo'
-import { StatusBar, Platform } from 'react-native'
-import Raven from 'raven-js'
+import { IntlProvider, addLocaleData } from 'react-intl'
 import { NavigationContext, NavigationProvider, StackNavigation, } from '@exponent/ex-navigation'
+import en from 'react-intl/locale-data/en'
+import pt from 'react-intl/locale-data/pt'
+import Raven from 'raven-js'
 import Router from '../config/Router'
 import apolloClient from '../config/apolloClient'
 require('raven-js/plugins/react-native')(Raven)
+
+addLocaleData([...en, ...pt])
+
+const locale = new Intl.DateTimeFormat().resolvedOptions().locale
 
 if (!__DEV__) {
   const DSN_KEY = 'https://5cdede7e751f4807b1113013db2d917b@sentry.io/87957'
@@ -23,11 +30,13 @@ class Root extends Component {
     const navigationContext = new NavigationContext({ router: Router, store: store })
 
     return (
-      <ApolloProvider store={store} client={apolloClient}>
-        <NavigationProvider context={navigationContext}>
-          <StackNavigation initialRoute={Router.getRoute('timeline')} />
-        </NavigationProvider>
-      </ApolloProvider>
+      <IntlProvider locale={locale}>
+        <ApolloProvider store={store} client={apolloClient}>
+          <NavigationProvider context={navigationContext}>
+            <StackNavigation initialRoute={Router.getRoute('timeline')} />
+          </NavigationProvider>
+        </ApolloProvider>
+      </IntlProvider>
     )
   }
 }
