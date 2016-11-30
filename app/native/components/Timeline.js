18bee254fa1b5be0a32022fca7a0faf9c280d32b
Klaus <klausy@gmail.com>
2016-11-30 13:10:20 +0000

    Fix infinite scroll if timeline doesn't have enough stories

--- app/native/components/Timeline.js
+++ app/native/components/Timeline.js
@@ -118,6 +118,9 @@ class Timeline extends Component {
   }
 
   onEndReached() {
+    const storiesArray = this.props.data.timeline.filter((item) => item.stories.length)
+    const minStoriesInTheViewport = 3
+    if (storiesArray.length < minStoriesInTheViewport) return
     //Reference: https://github.com/apollostack/react-apollo/issues/228
     this.setState({ loadingMore: true })
     this.props.data.infiniteScroll().then((data) => {
