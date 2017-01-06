a568a96474126a45a78a1d537334a84d0f8f54b4
Lucas <AgtLucas@users.noreply.github.com>
2017-01-06 13:23:33 -0200

    Update Android Splashscreen (#162)
    * Update Android Splashscreen
    
    * Fix color
    
    * Rename /java/com/mttrs to /java/garageborn/mttrs to avoid conflict with PR #161

---
+++ android/app/src/main/java/garageborn/mttrs/MainActivity.java
@@ -0,0 +1,37 @@
+package garageborn.mttrs;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.bridge.ReactContext;
+
+import com.facebook.react.ReactActivity;
+import com.BV.LinearGradient.LinearGradientPackage;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "mttrs";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // After RN start, set our background
+        getReactNativeHost().getReactInstanceManager().addReactInstanceEventListener(new ReactInstanceManager.ReactInstanceEventListener() {
+            @Override
+            public void onReactContextInitialized(ReactContext context) {
+                // Hide Splash Screen
+                getWindow().getDecorView().setBackgroundColor(Color.TRANSPARENT);
+            }
+        });
+
+        super.onCreate(savedInstanceState);
+    }
+}
