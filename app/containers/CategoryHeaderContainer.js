efca4d4ce0e8c2c440f899e614aeee5605b8891d
AgtLucas <lucas@agtlucas.com>
2017-01-19 12:05:45 -0200

    Refactor categorymenucontainer

--- app/containers/CategoryHeaderContainer.js
+++ app/containers/CategoryHeaderContainer.js
@@ -1,39 +1,13 @@
-import React, { Component, PropTypes } from 'react'
-import { Image } from 'react-native'
-import { NavigationActions } from '@exponent/ex-navigation'
+import React, { PropTypes } from 'react'
 import Header from '../components/Header'
-import styles from '../styles/Header'
-import * as cloudinary from '../common/utils/Cloudinary'
 
-class CategoryHeaderContainer extends Component {
-  get icon() {
-    return <Image style={styles.icon} source={{uri: this.getCategoryIcon()}} />
-  }
-
-  getCategoryIcon() {
-    const { icon_id } = this.props.category
-    if (!icon_id) return
-    let options = {
-      secure: true,
-      effect: 'colorize',
-      color: '#FFF',
-      width: 40,
-      height: 40,
-      crop: 'crop'
-    }
-    return cloudinary.id(icon_id, options)
-  }
-
-  render() {
-    const { toggleMenu, category } = this.props
-    return (
-      <Header
-        title={category.name}
-        toggleMenu={toggleMenu}
-        icon={this.icon}
-        />
-    )
-  }
+const CategoryHeaderContainer = ({ toggleMenu, category }) => {
+  return (
+    <Header
+      title={category.name}
+      toggleMenu={toggleMenu}
+    />
+  )
 }
 
 CategoryHeaderContainer.propTypes = {
