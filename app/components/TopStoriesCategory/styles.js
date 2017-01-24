2b148a505b66a03423399c7d18cb85dbc5bccb98
AgtLucas <lucas@agtlucas.com>
2017-01-24 11:39:47 -0200

    Update category tile

--- app/components/TopStoriesCategory/styles.js
+++ app/components/TopStoriesCategory/styles.js
@@ -2,6 +2,8 @@ import { StyleSheet, Dimensions } from 'react-native'
 
 const { width, height } = Dimensions.get('window')
 
+const tileWidth = width - 35
+
 const styles = StyleSheet.create({
   container: {
     marginHorizontal: width * 0.02,
@@ -10,19 +12,35 @@ const styles = StyleSheet.create({
 
   topStories: {
     backgroundColor: '#555',
-    width: width - 35,
+    width: tileWidth,
     height: height / 8,
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    justifyContent: 'center',
+
     borderBottomWidth: 2
   },
 
-  title: {
+  image: {
+    width: tileWidth,
+    height: 81,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    flexDirection: 'row'
+  },
+
+  name: {
+    backgroundColor: 'transparent',
     color: '#FFF',
     fontSize: 16,
     fontWeight: '500',
     marginBottom: 10
+  },
+
+  gradient: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    backgroundColor: 'transparent',
+    width: tileWidth,
+    height: height / 8
   }
 })
 
--- app/containers/CategoryMenuContainer.gql.js
+++ app/containers/CategoryMenuContainer.gql.js
@@ -1,7 +1,7 @@
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 
-const Query = gql`query { categories(ordered: true) { id name slug color icon_id } }`
+const Query = gql`query { categories(ordered: true) { id name slug color image_id } }`
 
 export default function (CategoryMenuContainer) {
   return graphql(Query)(CategoryMenuContainer)
