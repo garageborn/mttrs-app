ef28d5c1c45230191ba68b0c85553c29e3068fd4
Klaus Kohut <klausy@gmail.com>
2017-02-03 17:28:43 +0000

    Add translations

--- app/components/TimelineError/index.js
+++ app/components/TimelineError/index.js
@@ -1,27 +1,38 @@
 import React, { PropTypes } from 'react'
-import { View, Image, Text, Button } from 'react-native'
+import { View, Image, Text } from 'react-native'
 import { injectIntl, defineMessages } from 'react-intl'
 import Touchable from '../Touchable'
 import styles from './styles'
 
 const messages = defineMessages({
-  topStories: {
-    id: 'header.topStories',
-    defaultMessage: 'Top Stories'
+  textPrimary: {
+    id: 'disconnected.textPrimary',
+    defaultMessage: 'No internet connection'
+  },
+  textSecondary: {
+    id: 'disconnected.textSecondary',
+    defaultMessage: 'Try again when you are online'
+  },
+  buttonText: {
+    id: 'disconnected.buttonText',
+    defaultMessage: 'Reload'
   }
 })
 
-const TimelineError = ({ reloadTimeline }) => {
+const TimelineError = ({ intl, reloadTimeline }) => {
+  const textPrimary = intl.formatMessage(messages.textPrimary)
+  const textSecondary = intl.formatMessage(messages.textSecondary)
+  const buttonText = intl.formatMessage(messages.buttonText)
   return (
     <View style={styles.container}>
       <Image style={styles.bg} source={require('../../assets/disconnected-bg.png')} />
       <View>
-        <Image style={styles.icon} source={require('../../assets/icons/disconnected.png')}/>
-        <Text style={styles.textPrimary}>No Internet connection</Text>
-        <Text style={styles.textSecondary}>Try again when you are online</Text>
+        <Image style={styles.icon} source={require('../../assets/icons/disconnected.png')} />
+        <Text style={styles.textPrimary}>{textPrimary}</Text>
+        <Text style={styles.textSecondary}>{textSecondary}</Text>
         <Touchable onPress={reloadTimeline}>
           <View style={styles.buttonContainer}>
-            <Text style={styles.buttonText}>Reload</Text>
+            <Text style={styles.buttonText}>{buttonText}</Text>
           </View>
         </Touchable>
       </View>
