fc910d0c7b377d8a16b567d1b9aa2a851e553799
Alexandre Bini <alexandrebini@gmail.com>
2017-02-10 11:56:20 -0200

    image placeholder (#219)
    * image placeholder
    
    * image placeholder

--- app/components/StoryImage/index.js
+++ app/components/StoryImage/index.js
@@ -1,43 +1,73 @@
 import React, { Component, PropTypes } from 'react'
 import { Image, Platform } from 'react-native'
+import _sample from 'lodash/sample'
 import * as cloudinary from '../../common/utils/Cloudinary'
 import styles from './styles'
+
+const loading = require('../../assets/mttrs-loading.gif')
+const staticLoading = require('../../assets/mttrs-loading-static.png')
+const placeholders = [
+  require('./assets/placeholder-01.png'),
+  require('./assets/placeholder-02.png'),
+  require('./assets/placeholder-03.png'),
+  require('./assets/placeholder-04.png'),
+  require('./assets/placeholder-05.png')
+]
 
 class StoryImage extends Component {
   constructor () {
     super()
     this.handleImageLoad = this.handleImageLoad.bind(this)
-    this.state = {
-      imageLoaded: false
-    }
+    this.handleImageError = this.handleImageError.bind(this)
+    this.state = { status: 'loading' }
   }
 
-  getImage () {
-    if (!this.props.source) return
-    let options = { type: 'fetch', width: 240, height: 180, crop: 'fit', secure: true }
-    if (this.state.imageLoaded) {
-      return {uri: cloudinary.url(this.props.source, options)}
+  getSource () {
+    if (!this.props.source) return this.getPlaceholder()
+
+    if (this.state.status === 'loading') {
+      return this.getLoading()
     } else {
-      return Platform.select({
-        ios: require('../../assets/mttrs-loading.gif'),
-        android: require('../../assets/mttrs-loading-static.png')
-      })
+      return this.getImage()
     }
   }
 
   handleImageLoad () {
-    this.setState({imageLoaded: true})
+    this.setState({ status: 'loaded' })
+  }
+
+  handleImageError () {
+    this.setState({ status: 'error' })
   }
 
   render () {
+    if (this.state.status === 'error') return this.renderPlaceholder()
     return (
       <Image
         style={styles.image}
         onLoad={this.handleImageLoad}
+        onError={this.handleImageError}
         resizeMode='cover'
-        source={this.getImage()}
+        source={this.getSource()}
       />
     )
+  }
+
+  renderPlaceholder () {
+    return <Image style={styles.image} resizeMode='cover' source={this.getPlaceholder()} />
+  }
+
+  getImage () {
+    let options = { type: 'fetch', width: 240, height: 180, crop: 'fit', secure: true }
+    return { uri: cloudinary.url(this.props.source, options) }
+  }
+
+  getPlaceholder () {
+    return _sample(placeholders)
+  }
+
+  getLoading () {
+    return Platform.select({ ios: loading, android: staticLoading })
   }
 }
 
