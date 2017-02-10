3cea27732d9a3e53c9e3426e4544b61457f462ae
Alexandre Bini <alexandrebini@gmail.com>
2017-02-10 15:54:01 -0200

    Fix locale (#221)
    * Fix locale
    
    * Fix locale
    
    * Update IntlProvider.js
    
    * Update IntlProvider.js

--- app/config/IntlProvider.js
+++ app/config/IntlProvider.js
@@ -1,8 +1,7 @@
 import React, { Component, PropTypes } from 'react'
-import { Platform } from 'react-native'
+import { NativeModules, Platform } from 'react-native'
 import { IntlProvider, addLocaleData } from 'react-intl'
 import intl from 'intl'
-
 import * as messages from '../common/translations/i18n'
 import localeData from 'react-intl/locale-data'
 import androidLocaleData from 'intl/locale-data/complete'
@@ -15,12 +14,15 @@ Platform.select({
   }
 })
 
-export const locale = new Intl.DateTimeFormat().resolvedOptions().locale
+export const locale = Platform.select({
+  ios: () => NativeModules.SettingsManager.settings.AppleLocale,
+  android: () => NativeModules.I18nManager.localeIdentifier
+})()
+export const language = locale.substr(0, 2)
 export const timezone = new Intl.DateTimeFormat().resolvedOptions().timeZone
 
 class Provider extends Component {
   render () {
-    let language = locale.substr(0, 2)
     let msg = messages[language] || messages.en
 
     return (
