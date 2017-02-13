731d49092aa6f0a75845e77f80d29e577c17199b
Klaus Kohut <klausy@gmail.com>
2017-02-13 13:34:49 +0000

    Refactor date parser, add translations

--- app/components/ParsedDate/index.js
+++ app/components/ParsedDate/index.js
@@ -1,19 +1,58 @@
-import moment from './Moment'
+import React, {PropTypes, Component} from 'react'
+import { Text } from 'react-native'
+import moment from '../../common/utils/Moment'
+import { injectIntl, defineMessages } from 'react-intl'
 
-const ParseDate = (date) => {
-  switch (date) {
-    case 'last_week':
-      return 'Last Week'
-    case 'last_month':
-      return 'Last Month'
-    default:
-      return moment.unix(date).calendar(null, {
-        sameDay: '[Today]',
-        lastDay: '[Yesterday]',
-        lastWeek: 'MMMM D',
-        sameElse: 'MMMM D'
-      })
+const messages = defineMessages({
+  lastWeek: {
+    id: 'lastWeek'
+  },
+  lastMonth: {
+    id: 'lastMonth'
+  },
+  today: {
+    id: 'today'
+  },
+  yesterday: {
+    id: 'yesterday'
+  },
+  dateFormat: {
+    id: 'dateFormat'
+  }
+
+})
+
+class ParsedDate extends Component {
+  parser (date) {
+    let { intl } = this.props
+    switch (date) {
+      case 'last_week':
+        return intl.formatMessage(messages.lastWeek)
+      case 'last_month':
+        return intl.formatMessage(messages.lastMonth)
+      default:
+        return moment.unix(date).calendar(null, {
+          sameDay: `[${intl.formatMessage(messages.today)}]`,
+          lastDay: `[${intl.formatMessage(messages.yesterday)}]`,
+          lastWeek: intl.formatMessage(messages.dateFormat),
+          sameElse: intl.formatMessage(messages.dateFormat)
+        })
+    }
+  }
+
+  render () {
+    let { date } = this.props
+    let parsedDate = this.parser(date)
+    let text = parsedDate.toUpperCase()
+    return (
+      <Text style={this.props.style}>{text}</Text>
+    )
   }
 }
 
-export default ParseDate
+ParsedDate.propTypes = {
+  date: PropTypes.number,
+  style: PropTypes.number
+}
+
+export default injectIntl(ParsedDate)
