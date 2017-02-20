5e94108ea898f74159e82e14cd0edf8fc5dd1dd8
Alexandre Bini <alexandrebini@gmail.com>
2017-02-20 12:40:41 -0300

    iOS version 102 => 103 android version 115 => 116

--- app/constants/APIEndpoints.js
+++ app/constants/APIEndpoints.js
@@ -1,5 +1,4 @@
-// const ROOT = process.env.MTTRS_API_URL || 'https://api.mtt.rs'
-const ROOT = process.env.MTTRS_API_URL || 'http://localhost:3000'
+const ROOT = process.env.MTTRS_API_URL || 'https://api.mtt.rs'
 export const GRAPHQL = `${ROOT}/graphql`
 export const CATEGORIES = `${ROOT}/categories.json`
 export const PUBLISHERS = `${ROOT}/publishers.json`
