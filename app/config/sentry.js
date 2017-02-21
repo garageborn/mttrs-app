e6624cada66fc79d18b9e907d7d464498c95296e
Alexandre Bini <alexandrebini@gmail.com>
2017-02-21 18:03:40 -0300

    Features/sentry release (#254)
    * Sentry Release
    
    * Sentry Release
    
    * Sentry Release
    
    * iOS version 106 => 107
    
    * iOS version 107 => 108
    
    * iOS version 108 => 109
    
    * persist next release
    
    * persist next release
    
    * persist next release
    
    * persist next release
    
    * load version
    
    * Bump versions
    
    * iOS version 110 => 112
    
    * iOS version 112 => 113
    
    * iOS version 113 => 114
    
    * iOS version 114 => 115
    
    * desisto.
    
    * dSYM upload
    
    * iOS version 115 => 116 android version 119 => 120

--- app/config/sentry.js
+++ app/config/sentry.js
@@ -1,9 +1,16 @@
+import { Platform } from 'react-native'
 import Raven from 'raven-js'
 require('raven-js/plugins/react-native')(Raven)
+import release from './release.json'
+
+const currentRelease = Platform.select({
+  ios: release.ios,
+  android: release.android
+})
 
 if (!__DEV__) {
   const DSN_KEY = 'https://5cdede7e751f4807b1113013db2d917b@sentry.io/87957'
-  Raven.config(DSN_KEY, { release: '1.0' }).install()
+  Raven.config(DSN_KEY, { release: currentRelease }).install()
 }
 
 export default Raven
---
+++ fastlane/actions/get_android_release.rb
@@ -0,0 +1,35 @@
+module Fastlane
+  module Actions
+    class GetAndroidReleaseAction < Action
+      BUILD_FILE_PATH = File.expand_path('../../../android/app/build.gradle', __FILE__)
+      VERSION_CODE_MATCHER = /versionCode\s+(\d+)/.freeze
+      VERSION_NAME_MATCHER = /versionName\s+"(\d+\.\d+)"/.freeze
+
+      class << self
+        def run(_params = {})
+          UI.message 'Get Android Release'
+
+          { version_code: version_code, version_name: version_name }
+        end
+
+        def is_supported?(platform)
+          platform == :android
+        end
+
+        private
+
+        def version_code
+          build_file[VERSION_CODE_MATCHER, 1].to_i
+        end
+
+        def version_name
+          build_file[VERSION_NAME_MATCHER, 1]
+        end
+
+        def build_file
+          @build_file ||= File.read(BUILD_FILE_PATH)
+        end
+      end
+    end
+  end
+end
