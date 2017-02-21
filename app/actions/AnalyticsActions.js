a325b03905870628c42ee6792f92d83787932f71
Klaus Kohut <klausy@gmail.com>
2017-02-21 11:03:33 +0000

    Handle dev mode analytics

--- app/actions/AnalyticsActions.js
+++ app/actions/AnalyticsActions.js
@@ -1,14 +1,15 @@
 import analytics from '../config/Analytics'
 
 export function trackScreen (screen) {
-  return () => analytics.trackScreenView(screen)
+  return (dispatch) => {
+    console.log(analytics)
+    analytics.trackScreenView(screen)
+  }
 }
 
 export function trackLink (link) {
   return () => {
-    analytics.trackEvent(
-      link.url,
-      'Open'
-    )
+    console.log(analytics)
+    analytics.trackEvent(link.url, 'Open')
   }
 }
