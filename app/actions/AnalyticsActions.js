77e1569415e4b5e1e422839dee9499deb715487b
Klaus Kohut <klausy@gmail.com>
2017-02-21 11:16:20 +0000

    Lint

--- app/actions/AnalyticsActions.js
+++ app/actions/AnalyticsActions.js
@@ -1,15 +1,9 @@
 import analytics from '../config/Analytics'
 
 export function trackScreen (screen) {
-  return (dispatch) => {
-    console.log(analytics)
-    analytics.trackScreenView(screen)
-  }
+  return () => analytics.trackScreenView(screen)
 }
 
 export function trackLink (link) {
-  return () => {
-    console.log(analytics)
-    analytics.trackEvent(link.url, 'Open')
-  }
+  return () => analytics.trackEvent(link.url, 'Open')
 }
