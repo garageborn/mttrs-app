fc864e97791f56014ea71a712da6e2bc44bce47c
AgtLucas <lucas@agtlucas.com>
2017-02-24 07:28:12 -0300

    Fix KFormat

--- app/common/utils/KFormat.js
+++ app/common/utils/KFormat.js
@@ -1,5 +1,5 @@
 const KFormat = number => {
-  return number > 999 ? (number / 1000).toFixed(1) + 'K' : number
+  return number > 999 ? Math.floor((number / 1000).toFixed(1)) + 'K' : number
 }
 
 export default KFormat
