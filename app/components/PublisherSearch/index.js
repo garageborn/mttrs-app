645d152cb015757d656cdf2ead92562dbc9f0fd6
Lucas <AgtLucas@users.noreply.github.com>
2017-03-03 09:32:30 -0300

    Clear search button (#283)
    * Clear search button
    
    * Show clear button only when input has value
    
    * inputIsEmpty
    
    * Update icons

--- app/components/PublisherSearch/index.js
+++ app/components/PublisherSearch/index.js
@@ -1,7 +1,8 @@
-import React, { PropTypes } from 'react'
-import { View, TextInput, Image } from 'react-native'
+import React, { PropTypes, Component } from 'react'
+import { View, TextInput, Image, Platform, TouchableNativeFeedback } from 'react-native'
 import { injectIntl, defineMessages } from 'react-intl'
 import icon from './assets/icon.png'
+import close from './assets/close.png'
 import styles, { textColor } from './styles'
 
 const messages = defineMessages({
@@ -10,30 +11,59 @@ const messages = defineMessages({
   }
 })
 
-const PublisherSearch = ({ intl, onChangeText }) => {
-  let placeholder = intl.formatMessage(messages.searchPlaceholder)
+class PublisherSearch extends Component {
+  constructor () {
+    super()
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.search} shadowOffset={{width: 1, height: 2}} shadowColor={'rgba(0, 0, 0, .1)'} shadowOpacity={1.0}>
-        <Image style={styles.icon} source={icon} />
-        <TextInput
-          style={styles.searchInput}
-          underlineColorAndroid={'transparent'}
-          placeholder={placeholder}
-          placeholderTextColor={textColor}
-          onChangeText={onChangeText}
-        />
+    this.clear = this.clear.bind(this)
+  }
+
+  clear () {
+    this.refs.textInput.clear()
+    this.props.onCleanSearch()
+  }
+
+  clearButton () {
+    if (Platform.OS === 'ios' || this.props.inputIsEmpty) return
+    return (
+      <TouchableNativeFeedback onPress={this.clear}>
+        <View style={styles.close}>
+          <Image source={close} />
+        </View>
+      </TouchableNativeFeedback>
+    )
+  }
+
+  render () {
+    const { intl, onChangeText } = this.props
+    let placeholder = intl.formatMessage(messages.searchPlaceholder)
+    return (
+      <View style={styles.container}>
+        <View style={styles.search} shadowOffset={{width: 1, height: 2}} shadowColor={'rgba(0, 0, 0, .1)'} shadowOpacity={1.0}>
+          <Image style={styles.icon} source={icon} />
+          <TextInput
+            ref='textInput'
+            style={styles.searchInput}
+            underlineColorAndroid={'transparent'}
+            placeholder={placeholder}
+            placeholderTextColor={textColor}
+            onChangeText={onChangeText}
+            clearButtonMode='while-editing'
+          />
+          {this.clearButton()}
+        </View>
       </View>
-    </View>
-  )
+    )
+  }
 }
 
 PublisherSearch.propTypes = {
   intl: PropTypes.shape({
     formatMessage: PropTypes.func.isRequired
   }).isRequired,
-  onChangeText: PropTypes.func.isRequired
+  inputIsEmpty: PropTypes.bool.isRequired,
+  onChangeText: PropTypes.func.isRequired,
+  onCleanSearch: PropTypes.func.isRequired
 }
 
 export default injectIntl(PublisherSearch)
