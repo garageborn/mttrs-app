b409b0be651d61b7ca36dd07c885fa4d53b3cfcc
Klaus Kohut <klausy@gmail.com>
2017-03-03 10:37:22 +0000

    Add count

--- app/components/PublisherMenuItem/index.js
+++ app/components/PublisherMenuItem/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-no-bind */
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
 import { View, Text } from 'react-native'
 import Touchable from '../Touchable'
 import PublisherLogo from '../PublisherLogo'
@@ -7,25 +7,41 @@ import * as cloudinary from '../../common/utils/Cloudinary'
 import { WHITE_TRANSPARENT_COLOR } from '../../constants/TouchUnderlayColors'
 import styles from './styles'
 
-const PublisherMenuItem = ({ onPress, publisher }) => {
-  let renderIcon = () => {
+class PublisherMenuItem extends Component {
+  render () {
+    let { publisher, onPress } = this.props
+
+    return (
+      <Touchable
+        style={styles.touch}
+        onPress={e => onPress(publisher)}
+        underlayColor={WHITE_TRANSPARENT_COLOR}
+      >
+        <View style={styles.container}>
+          <View style={styles.leftContainer}>
+            {this.icon}
+            <Text style={styles.name}>{publisher.name}</Text>
+          </View>
+          <View style={styles.rightContainer}>
+            <Text style={styles.count}>{this.count}</Text>
+          </View>
+        </View>
+      </Touchable>
+    )
+  }
+
+  get icon () {
+    let { publisher } = this.props
     if (!publisher.icon_id) return
     const uri = cloudinary.id(publisher.icon_id, { secure: true })
     return <PublisherLogo size={30} source={{ uri }} />
   }
 
-  return (
-    <Touchable
-      style={styles.touch}
-      onPress={e => onPress(publisher)}
-      underlayColor={WHITE_TRANSPARENT_COLOR}
-    >
-      <View style={styles.publisher}>
-        {renderIcon()}
-        <Text style={styles.name}>{publisher.name}</Text>
-      </View>
-    </Touchable>
-  )
+  get count () {
+    let { publisher } = this.props
+    if (!publisher.todaysNews) return '--'
+    return publisher.todaysNews
+  }
 }
 
 PublisherMenuItem.propTypes = {
--- app/components/PublisherMenuItem/styles.js
+++ app/components/PublisherMenuItem/styles.js
@@ -1,14 +1,24 @@
 import { StyleSheet, Platform } from 'react-native'
 
 const styles = StyleSheet.create({
-  publisher: {
+  container: {
     backgroundColor: '#434341',
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
     height: 60,
     paddingHorizontal: 20,
     borderBottomWidth: StyleSheet.hairlineWidth,
     borderBottomColor: '#2D2D2B'
+  },
+
+  leftContainer: {
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+
+  rightContainer: {
+    marginRight: 12
   },
 
   name: {
@@ -23,6 +33,11 @@ const styles = StyleSheet.create({
 
   touch: {
     backgroundColor: '#F1F1F1'
+  },
+
+  count: {
+    fontSize: 14,
+    color: '#999999'
   }
 })
 
