22525b93478b2397b113236337360644870f1079
Alexandre Bini <alexandrebini@gmail.com>
2017-03-27 11:47:14 -0300

    Analytics heart beat

--- app/actions/AnalyticsActions.js
+++ app/actions/AnalyticsActions.js
@@ -1,5 +1,12 @@
 import analytics from '../config/Analytics'
 
 export function trackScreen (screen) {
+  console.log('trackScreen', screen)
   return () => analytics.trackScreenView(screen)
 }
+
+export function trackEvent (category, action, optionalValues = {}) {
+  console.log('trackEvent', category, action)
+  return () => analytics.trackEvent(category, action, optionalValues)
+}
+
