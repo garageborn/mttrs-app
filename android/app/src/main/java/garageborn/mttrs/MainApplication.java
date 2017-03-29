66bb1a6da3541d87562bc2fda3474c4a7e4507ae
Klaus Kohut <klausy@gmail.com>
2017-03-29 17:24:32 +0100

    Works on android

--- android/app/src/main/java/garageborn/mttrs/MainApplication.java
+++ android/app/src/main/java/garageborn/mttrs/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
 import cl.json.RNSharePackage;
 import com.facebook.react.ReactInstanceManager;
@@ -37,6 +38,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
           new GoogleAnalyticsBridgePackage(),
           new RNSharePackage(),
           new LinearGradientPackage(),
