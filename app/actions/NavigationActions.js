8b4a176e29e557eb7f92bec01ceb6c1f84cac44e
Klaus Kohut <klausy@gmail.com>
2017-04-24 15:55:10 +0100

    Isolate handleRoute method

--- app/actions/NavigationActions.js
+++ app/actions/NavigationActions.js
@@ -14,8 +14,8 @@ export function home () {
     let menuParams = Object.assign({}, params.menu, { open: false })
     let sectionParams = Object.assign({}, params.section, { name: 'home', model: {} })
     let newParams = Object.assign({}, params, { section: sectionParams, menu: menuParams })
-    dispatch(NavigationActions.updateCurrentRouteParams(navigation.currentNavigatorUID, newParams))
     dispatch(AnalyticsActions.trackScreen(`/${sectionParams.name}`))
+    return dispatch(handleRoute(newParams))
   }
 }
 
@@ -36,9 +36,8 @@ export function selectCategory (category) {
 
     let sectionParams = Object.assign({}, params.section, { name: 'category', model: category })
     let newParams = Object.assign({}, params, { section: sectionParams })
-
-    dispatch(NavigationActions.updateCurrentRouteParams(navigation.currentNavigatorUID, newParams))
     dispatch(AnalyticsActions.trackScreen(`/${sectionParams.model.slug}`))
+    return dispatch(handleRoute(newParams))
   }
 }
 
@@ -51,14 +50,8 @@ export function selectPublisher (publisher) {
     let menuParams = Object.assign({}, params.menu, { open: false })
     let sectionParams = Object.assign({}, params.section, { name: 'publisher', model: publisher })
     let newParams = Object.assign({}, params, { section: sectionParams, menu: menuParams })
-    const currentRoute = getCurrentRoute(getState)
-    const route = Router.getRoute('timeline', newParams)
-    if (currentRoute.routeName === 'link') {
-      dispatch(NavigationActions.push(navigation.currentNavigatorUID, route))
-    } else {
-      dispatch(NavigationActions.updateCurrentRouteParams(navigation.currentNavigatorUID, newParams))
-    }
     dispatch(AnalyticsActions.trackScreen(`/${sectionParams.model.slug}`))
+    return dispatch(handleRoute(newParams))
   }
 }
 
@@ -70,11 +63,9 @@ export function storyLinks (storyLinksParams) {
 
     let sectionParams = Object.assign({}, params.section, { storyLinks: storyLinksParams })
     let newParams = Object.assign({}, params, { section: sectionParams })
-
-    dispatch(NavigationActions.updateCurrentRouteParams(navigation.currentNavigatorUID, newParams))
-
     let linkSlug = _result(storyLinksParams, 'story.main_link.slug')
     if (linkSlug) dispatch(AnalyticsActions.trackScreen(`/link/${linkSlug}/story-links`))
+    return dispatch(handleRoute(newParams))
   }
 }
 
@@ -82,6 +73,16 @@ export function back () {
   return (dispatch, getState) => {
     const navigation = getNavigation(getState)
     dispatch(NavigationActions.pop(navigation.currentNavigatorUID))
+  }
+}
+
+function handleRoute (newParams) {
+  return (dispatch, getState) => {
+    const currentRoute = getCurrentRoute(getState)
+    const navigation = getNavigation(getState)
+    const route = Router.getRoute('timeline', newParams)
+    if (currentRoute.routeName === 'link') return dispatch(NavigationActions.push(navigation.currentNavigatorUID, route))
+    return dispatch(NavigationActions.updateCurrentRouteParams(navigation.currentNavigatorUID, newParams))
   }
 }
 
