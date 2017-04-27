07e2a1aa4f1b4d3eeb55e5b211433fc0584f7100
Klaus <klausy@gmail.com>
2017-04-27 16:55:13 +0100

    Add styles to Ads (#349)

--- app/components/TimelineAd/index.js
+++ app/components/TimelineAd/index.js
@@ -2,32 +2,38 @@ import React, { PropTypes, Component } from 'react'
 import { Image, Text, View } from 'react-native'
 import { withNativeAd } from 'react-native-fbads'
 import _isEmpty from 'lodash/isEmpty'
+import AdMain from './components/AdMain'
+import AdImage from './components/AdImage'
+import AdFooter from './components/AdFooter'
 import styles from './styles'
 import Button from '../Button'
 
 class TimelineAd extends Component {
   render () {
     const { nativeAd } = this.props
-    if (_isEmpty(nativeAd)) return
+    if (_isEmpty(nativeAd)) return null
 
     const { callToActionText, coverImage, description, icon, subtitle, title } = nativeAd
 
     return (
-      <View>
-        <Image source={{uri: icon}}/>
-        <Text>{title}</Text>
-        <Text>{subtitle}</Text>
-        <Image source={{uri: coverImage}}/>
-        <Text>{description}</Text>
-        <Button onPress={() => {}} skin={styles.button}>
-          <Text>{callToActionText}</Text>
-        </Button>
+      <View style={styles.container}>
+        <AdMain title={title} subtitle={subtitle} icon={icon} />
+        <AdImage source={coverImage} />
+        <AdFooter description={description} buttonText={callToActionText} />
       </View>
     )
   }
 }
 
 TimelineAd.propTypes = {
+  nativeAd: PropTypes.shape({
+    callToActionText: PropTypes.string,
+    coverImage: PropTypes.string,
+    description: PropTypes.string,
+    icon: PropTypes.string,
+    subtitle: PropTypes.string,
+    title: PropTypes.string
+  })
 }
 
 export default withNativeAd(TimelineAd)
