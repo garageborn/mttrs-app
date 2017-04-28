ac719e7c9ee41e3c7a9fd3bc51e1f0529750c8e1
Alexandre Bini <alexandrebini@gmail.com>
2017-04-28 18:14:05 -0300

    remove test devices

--- app/containers/TimelineAdContainer/index.js
+++ app/containers/TimelineAdContainer/index.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-import { NativeAdsManager, AdSettings } from 'react-native-fbads'
+import { NativeAdsManager } from 'react-native-fbads'
 import TimelineAd from '../../components/TimelineAd'
 import { TIMELINE_AD_PLACEMENT_ID } from '../../constants/Ads'
 
 class TimelineAdContainer extends Component {
   constructor (props) {
     super(props)
-    // if (this.__DEV__) AdSettings.addTestDevice(AdSettings.currentDeviceHash)
-    AdSettings.addTestDevice(AdSettings.currentDeviceHash)
 
     this.state = {
       adsManager: new NativeAdsManager(TIMELINE_AD_PLACEMENT_ID, 1)
