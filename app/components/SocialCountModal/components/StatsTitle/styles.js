67e780642cb92094a0fc42c6bd0751ab1417669c
Klaus Kohut <klausy@gmail.com>
2017-05-02 15:38:06 +0100

    Add shares title

---
+++ app/components/SocialCountModal/components/StatsTitle/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#999',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: '500'
+  },
+  textHighlight: {
+    color: '#666766',
+    fontSize: 24,
+    paddingHorizontal: 10
+  }
+})
+
+export default styles
--- app/components/SocialCountModal/index.js
+++ app/components/SocialCountModal/index.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import { View } from 'react-native'
-import styles from '../../styles/StoryLinks'
+import styles from './styles'
+import StatsTitle from './components/StatsTitle'
 import StoryLinkContainer from '../../containers/StoryLinkContainer'
 
 class SocialCountModal extends Component {
@@ -8,15 +9,26 @@ class SocialCountModal extends Component {
     if (!this.mainLink) return
     return (
       <View style={styles.container}>
-        <View style={styles.header}>
+        <View style={styles.storyLinkContainer}>
           <StoryLinkContainer
             type='header'
             story={this.props.story}
             link={this.mainLink}
           />
         </View>
+        <View style={styles.statsContainer}>
+          <StatsTitle linkCount={this.linkCount} totalCount={this.totalCount} />
+        </View>
       </View>
     )
+  }
+
+  get linkCount () {
+    return this.props.story.other_links.length + 1
+  }
+
+  get totalCount () {
+    return this.props.story.social_counter.total
   }
 
   get mainLink () {
@@ -26,7 +38,11 @@ class SocialCountModal extends Component {
 
 SocialCountModal.propTypes = {
   story: PropTypes.shape({
-    main_link: PropTypes.object.isRequired
+    main_link: PropTypes.object.isRequired,
+    other_links: PropTypes.array.isRequired,
+    social_counter: PropTypes.shape({
+      total: PropTypes.number.isRequired
+    })
   }).isRequired
 }
 
