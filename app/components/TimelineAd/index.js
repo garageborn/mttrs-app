6d5da27bee19425fb605be2d47b558c0901de8c7
Alexandre Bini <alexandrebini@gmail.com>
2017-05-04 16:37:33 -0300

    Features/admob2 (#356)
    * Timeline ad
    
    * Add styles to Ads (#349)
    
    * ad position
    
    * ads constant
    
    * all devices
    
    * Merge branch 'master' into features/timelinead
    
    * Fix ad styles
    
    * remove test devices
    
    * satanas
    
    * satanas
    
    * lucifer
    
    * remove fbads!
    
    * lucifer
    
    * using admob from github
    
    * remove timelinecontainer
    
    * ad size
    
    * Fix styles
    
    * android version 195 => 196
    
    * useless i18n

---
+++ app/components/TimelineAd/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { AdMobNativeExpress } from 'react-native-admob'
+import { ADMOB_TIMELINE_AD_ID } from '../../constants/Ads'
+import styles, { bannerWidth, bannerHeight } from './styles'
+
+class TimelineAd extends Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <AdMobNativeExpress
+          adUnitID={ADMOB_TIMELINE_AD_ID}
+          adViewDidReceiveAd={ (e) => console.log('adViewDidReceiveAd', e) }
+          didFailToReceiveAdWithError={ (e) => console.log('didFailToReceiveAdWithError', e) }
+          adViewWillPresentScreen={ (e) => console.log('adViewWillPresentScreen', e) }
+          adViewWillDismissScreen={ (e) => console.log('adViewWillDismissScreen', e) }
+          adViewDidDismissScreen={ (e) => console.log('adViewDidDismissScreen', e) }
+          adViewWillLeaveApplication={ (e) => console.log('adViewWillLeaveApplication', e) }
+          bannerWidth={bannerWidth}
+          bannerHeight={bannerHeight}
+        />
+      </View>
+    )
+  }
+}
+
+export default TimelineAd
