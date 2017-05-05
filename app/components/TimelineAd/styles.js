c1f9a06f813e729b895c96eab10e1456e06234bd
Klaus Kohut <klausy@gmail.com>
2017-05-05 15:58:46 +0100

    Fix ad styles

--- app/components/TimelineAd/styles.js
+++ app/components/TimelineAd/styles.js
@@ -2,10 +2,10 @@ import { StyleSheet, Dimensions } from 'react-native'
 let { width } = Dimensions.get('window')
 
 export const bannerWidth = width - 40
-export const bannerHeight = 320
+export const bannerHeight = width * 0.92
 const styles = StyleSheet.create({
   container: {
-    height: bannerHeight - 10,
+    height: bannerHeight - 15,
     position: 'relative',
     alignItems: 'center',
     justifyContent: 'flex-start',
