842128f9b90556e124da8222af884dc59f342082
Alexandre Bini <alexandrebini@gmail.com>
2017-05-11 19:57:18 -0300

    revmob

--- android/app/src/main/java/garageborn/mttrs/MainApplication.java
+++ android/app/src/main/java/garageborn/mttrs/MainApplication.java
@@ -3,7 +3,7 @@ package garageborn.mttrs;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.sbugert.rnadmob.RNAdMobPackage;
+import com.rctrevmob.RevMobPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -44,6 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RevMobPackage(),
           new ReactNativeOneSignalPackage(),
           new GoogleAnalyticsBridgePackage(),
           new RNSharePackage(),
--- app/components/TimelineAd/index.js
+++ app/components/TimelineAd/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Platform, View } from 'react-native'
-import { AdMobNativeExpress } from 'react-native-admob'
 import { ADMOB_TIMELINE_AD_ID } from '../../constants/Ads'
 import captureError from '../../common/utils/captureError'
 import styles, { bannerWidth, bannerHeight } from './styles'
@@ -21,12 +20,6 @@ class TimelineAd extends Component {
     if (this.state.error) return null
     return (
       <View style={styles.container}>
-        <AdMobNativeExpress
-          adUnitID={ADMOB_TIMELINE_AD_ID}
-          bannerWidth={bannerWidth}
-          bannerHeight={bannerHeight}
-          didFailToReceiveAdWithError={this.handleError}
-        />
       </View>
     )
   }
