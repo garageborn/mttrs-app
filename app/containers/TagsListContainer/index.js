7bde9e06cc43fc07eaa72e9dce5e594113d2ab73
Alexandre Bini <alexandrebini@gmail.com>
2017-05-24 18:15:11 -0300

    Tag list

--- app/containers/TagsListContainer/index.js
+++ app/containers/TagsListContainer/index.js
@@ -1,28 +1,20 @@
 import React, { Component, PropTypes} from 'react'
 import TagsList from '../../components/TagsList'
 import withQuery from './index.gql'
-import _isNil from 'lodash/isNil'
+import _result from 'lodash/isNil'
 
 class TagsListContainer extends Component {
   render () {
-    const { data, active, handleTag, menuOpen } = this.props
-    if (_isNil(data.tags) || !data.tags.length) return null
-    return (
-      <TagsList
-        active={active}
-        handleTag={handleTag}
-        data={data}
-        menuOpen={menuOpen}
-      />
-    )
+    const { data, active, handleTag } = this.props
+    if (!_result(data, 'tags.length')) return null
+    return <TagsList active={active} handleTag={handleTag} data={data} />
   }
 }
 
 TagsListContainer.propTypes = {
   data: PropTypes.object.isRequired,
   handleTag: PropTypes.func.isRequired,
-  active: PropTypes.string,
-  menuOpen: PropTypes.bool.isRequired
+  active: PropTypes.string
 }
 
 export default withQuery(TagsListContainer)
