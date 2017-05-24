c3e6cded6d00365e97244b4189a175ba783d0b2c
Alexandre Bini <alexandrebini@gmail.com>
2017-05-24 11:48:48 -0300

    timeline filter

--- app/scenes/FavoritesTimelineScene/index.js
+++ app/scenes/FavoritesTimelineScene/index.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-
-import { Text, View } from 'react-native'
-import { NavigationActions } from '../../actions/index'
-import Touchable from '../../components/Touchable'
-
+import { View } from 'react-native'
 import FavoriteCategoriesDialogContainer from '../../containers/FavoriteCategoriesDialogContainer'
 import FavoritesTimelineContainer from '../../containers/FavoritesTimelineContainer'
 import FavoritePublishersSelectorContainer from '../../containers/FavoritePublishersSelectorContainer'
@@ -15,14 +10,10 @@ class FavoritesTimelineScene extends Component {
       <View>
         <FavoriteCategoriesDialogContainer />
         <FavoritePublishersSelectorContainer />
-        <Touchable onPress={()=>this.props.dispatch(NavigationActions.addFavorites())}>
-          <Text>AddFuckingPublishers</Text>
-        </Touchable>
         <FavoritesTimelineContainer />
       </View>
     )
   }
 }
 
-// export default FavoritesTimelineScene
-export default connect()(FavoritesTimelineScene)
+export default FavoritesTimelineScene
