0d2b6c7b48b4fa93006905841ba98caa3ee36f6c
Klaus <klausy@gmail.com>
2017-05-25 18:25:38 +0200

    Features/publisher header (#399)
    * Revert babel, ios project
    
    * Add publisher header, custom back button

---
+++ app/components/HeaderLeft/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/jsx-no-bind */
+import React, { PropTypes } from 'react'
+import HeaderButton from '../HeaderButton'
+
+const HeaderLeft = ({ navigation }) => (
+  <HeaderButton type='back' onPress={() => this.props.navigation.goBack()} />
+)
+
+HeaderLeft.propTypes = {
+  navigation: PropTypes.shape({
+    goBack: PropTypes.func.isRequired
+  }).isRequired
+}
+
+export default HeaderLeft
