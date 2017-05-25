62cd1927215f606644915d8631f76f8015584440
Alexandre Bini <alexandrebini@gmail.com>
2017-05-25 05:39:37 -0300

    Empty Favorites Timeline

--- app/scenes/FavoritesTimelineScene/index.js
+++ app/scenes/FavoritesTimelineScene/index.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
+import React, { Component, PropTypes } from 'react'
+import { ActivityIndicator, View } from 'react-native'
+import { connect } from 'react-redux'
 import FavoriteCategoriesDialogContainer from '../../containers/FavoriteCategoriesDialogContainer'
 import FavoritesTimelineContainer from '../../containers/FavoritesTimelineContainer'
 import FavoritePublishersSelectorContainer from '../../containers/FavoritePublishersSelectorContainer'
+import EmptyFavoritesTimelineContainer from '../../containers/EmptyFavoritesTimelineContainer'
+import { FavoritePublishersActions } from '../../actions/index'
 
 class FavoritesTimelineScene extends Component {
+  componentWillMount () {
+    this.props.dispatch(FavoritePublishersActions.getPublishers())
+  }
+
   render () {
+    const { isLoaded, exists } = this.props
+    if (!isLoaded) return this.renderLoading()
+    if (!exists) return <EmptyFavoritesTimelineContainer />
+
     return (
       <View>
         <FavoriteCategoriesDialogContainer />
@@ -14,6 +25,26 @@ class FavoritesTimelineScene extends Component {
       </View>
     )
   }
+
+  renderLoading () {
+    return (
+      <View>
+        <ActivityIndicator size='large' color='#AAA' />
+      </View>
+    )
+  }
 }
 
-export default FavoritesTimelineScene
+FavoritesTimelineScene.propTypes = {
+  isLoaded: PropTypes.bool.isRequired,
+  exists: PropTypes.bool.isRequired
+}
+
+let mapStateToProps = (state, ownProps) => {
+  return {
+    isLoaded: state.FavoritePublishersReducer.isLoaded,
+    exists: state.FavoritePublishersReducer.items.length > 0
+  }
+}
+
+export default connect(mapStateToProps)(FavoritesTimelineScene)
