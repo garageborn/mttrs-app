fa3e81cf66065a7a94471e3803e52f628c263999
Alexandre Bini <alexandrebini@gmail.com>
2017-05-26 17:44:36 -0300

    react-native-sentry

--- app/config/sentry.js
+++ app/config/sentry.js
@@ -7,9 +7,13 @@ const currentRelease = Platform.select({
   android: release.android
 })
 
-// if (!__DEV__) {
-  const DSN_KEY = 'https://5cdede7e751f4807b1113013db2d917b@sentry.io/87957'
-  Sentry.config(DSN_KEY, { release: currentRelease }).install()
-// }
+const dnsKey = Platform.select({
+  ios: 'https://5409c6397ca84bf890d111fa5d505875:b0bb65b6c8ba42eabffb4e0821bb055f@sentry.io/172813',
+  android: 'https://18690aa15041497eb618da54dd976bbf:f8da59dea10d4748bf1726dfbaa92ce4@sentry.io/172809'
+})
+
+if (!__DEV__) {
+  Sentry.config(dnsKey, { release: currentRelease }).install()
+}
 
 export default Sentry
