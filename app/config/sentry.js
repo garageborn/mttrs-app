5bf768a31adaf92861b77158e5167b1070b37aa1
Alexandre Bini <alexandrebini@gmail.com>
2017-05-26 17:44:36 -0300

    react-native-sentry

--- app/config/sentry.js
+++ app/config/sentry.js
@@ -1,6 +1,5 @@
 import { Platform } from 'react-native'
-import Raven from 'raven-js'
-require('raven-js/plugins/react-native')(Raven)
+import { Sentry } from 'react-native-sentry'
 import release from './release.json'
 
 const currentRelease = Platform.select({
@@ -8,9 +7,9 @@ const currentRelease = Platform.select({
   android: release.android
 })
 
-if (!__DEV__) {
+// if (!__DEV__) {
   const DSN_KEY = 'https://5cdede7e751f4807b1113013db2d917b@sentry.io/87957'
-  Raven.config(DSN_KEY, { release: currentRelease }).install()
-}
+  Sentry.config(DSN_KEY, { release: currentRelease }).install()
+// }
 
-export default Raven
+export default Sentry
--- ios/mttrs/AppDelegate.m
+++ ios/mttrs/AppDelegate.m
@@ -11,6 +11,11 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#if __has_include(<React/RNSentry.h>)
+#import <React/RNSentry.h> // This is used for versions of react >= 0.40
+#else
+#import "RNSentry.h" // This is used for versions of react < 0.40
+#endif
 
 #import <Fabric/Fabric.h>
 #import <Crashlytics/Crashlytics.h>
@@ -37,6 +42,9 @@
                                                       moduleName:@"mttrs"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
+
+  [RNSentry installWithRootView:rootView];
+
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
