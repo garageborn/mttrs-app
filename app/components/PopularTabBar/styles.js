627fa24fc1b6595cd3e1b4688e539d8d6f96edda
Alexandre Bini <alexandrebini@gmail.com>
2017-05-28 10:31:38 -0300

    tabbar style

---
+++ app/components/PopularTabBar/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center'
+  },
+  text: {
+    color: '#999999',
+    textAlign: 'center'
+  }
+})
+
+export default styles
