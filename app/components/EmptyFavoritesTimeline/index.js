421b0e679dbdca6fd3347affda833655d4acd12b
Klaus <klausy@gmail.com>
2017-05-29 20:09:14 +0200

    Add addFavorites splash styles (#423)

--- app/components/EmptyFavoritesTimeline/index.js
+++ app/components/EmptyFavoritesTimeline/index.js
@@ -1,7 +1,12 @@
 import React, { Component, PropTypes } from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 import { injectIntl, defineMessages } from 'react-intl'
 import Button from '../Button'
+import Heading from '../Heading'
+import styles, { headingColor } from './styles'
+
+const starIcon = require('./assets/star.png')
+const bg = require('./assets/bg.png')
 
 const messages = defineMessages({
   button: { id: 'emptyFavoritesTimeline.button' },
@@ -13,14 +18,24 @@ class EmptyFavoritesTimeline extends Component {
     const { intl, onPress } = this.props
 
     return (
-      <View>
-        <Text>{intl.formatMessage(messages.heading)}</Text>
-        <Button
-          background='danger'
-          content={intl.formatMessage(messages.button)}
-          onPress={onPress}
-          size='regular'
-        />
+      <View style={styles.container}>
+        <Image style={styles.bg} source={bg} />
+        <View style={styles.content}>
+          <View style={styles.headingContainer}>
+            <Heading textStyle={styles.textStyle} size='small' color={headingColor}>
+              {intl.formatMessage(messages.heading)}
+            </Heading>
+          </View>
+          <Image style={styles.icon} source={starIcon} />
+          <View style={styles.buttonContainer}>
+            <Button
+              background='danger'
+              content={intl.formatMessage(messages.button)}
+              onPress={onPress}
+              size='regular'
+            />
+          </View>
+        </View>
       </View>
     )
   }
