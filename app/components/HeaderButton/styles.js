bd1ea8a069d07aaf17b3f65ea30df7a54acd1ab9
Klaus <klausy@gmail.com>
2017-05-29 12:20:05 +0100

    Update styles.js

--- app/components/HeaderButton/styles.js
+++ app/components/HeaderButton/styles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 16,
+    paddingHorizontal: 10,
     paddingVertical: 10
   }
 })
