86a1854603738b725ee11d00756a5dbbfb5c1a98
Klaus Kohut <klausy@gmail.com>
2017-05-31 13:22:18 +0100

    Fix review app link

--- app/containers/RateAppSettingsContainer/index.js
+++ app/containers/RateAppSettingsContainer/index.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
 import { Linking, Platform } from 'react-native'
-import * as StoreReview from 'react-native-store-review'
 import RateAppSettings from '../../components/RateAppSettings'
 
 const storeLink = Platform.select({
   ios: {
-    native: 'itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=1166566093',
-    external: 'https://itunes.apple.com/app/matters-news-summaries/id1166566093'
+    external: 'https://itunes.apple.com/pt/app/matters-resumo-de-not%C3%ADcias/id1166566093?mt=8'
   },
   android: {
-    native: 'market://details?id=garageborn.mttrs',
     external: 'https://play.google.com/store/apps/details?id=garageborn.mttrs'
   }
 })
@@ -17,29 +14,14 @@ const storeLink = Platform.select({
 class RateAppSettingsContainer extends Component {
   constructor () {
     super()
-    this.rateApp = this.rateApp.bind(this)
+    this.openUrl = this.openUrl.bind(this)
   }
 
   render () {
-    return <RateAppSettings onPress={this.rateApp} />
+    return <RateAppSettings onPress={this.openUrl} />
   }
 
-  rateApp () {
-    if (StoreReview.isAvailable) return StoreReview.requestReview()
-    this.openNative()
-  }
-
-  openNative () {
-    Linking.canOpenURL(storeLink.native).then(supported => {
-      if (supported) {
-        Linking.openURL(storeLink.native)
-      } else {
-        this.openExternal()
-      }
-    }).catch()
-  }
-
-  openExternal () {
+  openUrl () {
     Linking.canOpenURL(storeLink.external).then(supported => {
       if (supported) Linking.openURL(storeLink.external)
     }).catch()
