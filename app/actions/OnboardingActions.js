9f57669e6b94cb45ddb2ca2cb1fa6d4b63507ca5
Alexandre Bini <alexandrebini@gmail.com>
2017-06-20 10:52:08 -0300

    fuck

--- app/actions/OnboardingActions.js
+++ app/actions/OnboardingActions.js
@@ -28,6 +28,7 @@ export function getOnboardingStatus () {
     dispatch(this.requestOnboarding())
     AsyncStorage.getItem('showOnboarding', (error, data) => {
       if (error) captureError(error)
+      return dispatch(this.showOnboarding(true))
 
       if (data === null) {
         dispatch(this.showOnboarding(true))
