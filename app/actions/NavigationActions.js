e40cba131f4f5260d3e2867b89ada8e3cdb5c9e9
Alexandre Bini <alexandrebini@gmail.com>
2017-06-28 10:44:26 -0300

    Features/fix change country (#482)
    * es translations by ettore cabron
    
    * fix change country

--- app/actions/NavigationActions.js
+++ app/actions/NavigationActions.js
@@ -117,12 +117,3 @@ export function publisherCategoriesDialog (publisher, content) {
 export function closeModal () {
   return dispatch => dispatch(UIActions.closeModal())
 }
-
-export function reset () {
-  return dispatch => dispatch(NavigationActions.reset({
-    index: 0,
-    actions: [
-      NavigationActions.navigate({ routeName: 'timeline' })
-    ]
-  }))
-}
