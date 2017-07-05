075d98539d225a28bec7ef8c316eb94c647a9f73
Klaus <klausy@gmail.com>
2017-07-05 15:50:02 +0100

    Features/add es language (#488)
    * Get country by locale
    
    * Add es as language, add flag icons
    
    * Add review suggestions
    
    * Add findByCountryAndLanguage

--- app/common/utils/Moment.js
+++ app/common/utils/Moment.js
@@ -1,6 +1,7 @@
 import moment from 'moment-timezone'
 import _includes from 'lodash/includes'
 import 'moment/locale/pt'
+import 'moment/locale/es'
 import { AVAILABLE_LANGUAGES, DEFAULT_LANGUAGE, TIMEZONE } from '../../constants/Locale'
 
 export default function (language) {
