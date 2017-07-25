405c5d895743bcb482de5f219cc8432c92b85735
Klaus <klausy@gmail.com>
2017-07-25 12:01:34 +0100

    Remove google plus (#502)

--- app/components/SocialCountModal/components/StatsSocialWrapper/index.js
+++ app/components/SocialCountModal/components/StatsSocialWrapper/index.js
@@ -5,22 +5,16 @@ import styles from './styles'
 
 const StatsSocialWrapper = ({ socialCounter }) => (
   <View style={styles.container}>
-    <View style={styles.firstRow}>
-      <StatsSocialItem type='facebook' value={socialCounter.facebook} />
-      <StatsSocialItem type='twitter' value={socialCounter.twitter} />
-      <StatsSocialItem type='linkedin' value={socialCounter.linkedin} />
-    </View>
-    <View style={styles.secondRow}>
-      <StatsSocialItem type='pinterest' value={socialCounter.pinterest} />
-      <StatsSocialItem type='googlePlus' value={socialCounter.google_plus} />
-    </View>
+    <StatsSocialItem type='facebook' value={socialCounter.facebook} />
+    <StatsSocialItem type='twitter' value={socialCounter.twitter} />
+    <StatsSocialItem type='linkedin' value={socialCounter.linkedin} />
+    <StatsSocialItem type='pinterest' value={socialCounter.pinterest} />
   </View>
 )
 
 StatsSocialWrapper.propTypes = {
   socialCounter: PropTypes.shape({
     facebook: PropTypes.number.isRequired,
-    google_plus: PropTypes.number.isRequired,
     linkedin: PropTypes.number.isRequired,
     pinterest: PropTypes.number.isRequired,
     twitter: PropTypes.number.isRequired
--- app/components/SocialCountModal/components/StatsSocialWrapper/styles.js
+++ app/components/SocialCountModal/components/StatsSocialWrapper/styles.js
@@ -3,19 +3,12 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     borderColor: '#DADADA',
     borderBottomWidth: 1,
-    paddingVertical: 10
-  },
-  firstRow: {
+    padding: 10,
     flexDirection: 'row',
     marginBottom: 10
-  },
-  secondRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginHorizontal: 40
   }
 })
 
